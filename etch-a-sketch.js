36533dbebd92d59ea5939fb4914f3998b54090af
Vaibhav Vishwakarma <vaibhav.vishwa1309@gmail.com>
2025-10-26 02:18:02 +0530

    add div color change feature on mouse hover

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,6 +3,19 @@ let containerDiv = document.querySelector("#container");
  
 for (let i=0; i<256; i++) {
     let createdDiv =document.createElement("div");
-    createdDiv.classList.add("gridDiv")
+    createdDiv.classList.add("gridDiv");
+
+
+
+    createdDiv.addEventListener('mouseenter', (e) => {
+    e.target.style.backgroundColor = 'green';});
+
+    // createdDiv.addEventListener('mouseleave', (e) => {
+    // e.target.style.backgroundColor = 'red';});
+
+
     containerDiv.appendChild(createdDiv);
+
 }
+
+
