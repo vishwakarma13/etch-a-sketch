1355c3d7acd952509af88d4a09f561172b4a2820
Vaibhav Vishwakarma <vaibhav.vishwa1309@gmail.com>
2025-10-25 20:47:13 +0530

    add class to created div

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,5 +3,6 @@ let containerDiv = document.querySelector("#container");
  
 for (let i=0; i<256; i++) {
     let createdDiv =document.createElement("div");
+    createdDiv.classList.add("gridDiv")
     containerDiv.appendChild(createdDiv);
 }
