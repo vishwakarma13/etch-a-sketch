9fd5ed45a3186b550a199106c1b1508021c2a8b5
Vaibhav Vishwakarma <vaibhav.vishwa1309@gmail.com>
2025-10-25 20:37:19 +0530

    add code for div creation in container div

---
+++ etch-a-sketch.js
@@ -0,0 +1,7 @@
+let containerDiv = document.querySelector("#container");
+
+ 
+for (let i=0; i<256; i++) {
+    let createdDiv =document.createElement("div");
+    containerDiv.appendChild(createdDiv);
+}
