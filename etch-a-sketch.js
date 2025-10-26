b38546f8a81c52a61f866c5c6d717f657b0772e6
Vaibhav Vishwakarma <vaibhav.vishwa1309@gmail.com>
2025-10-26 19:57:48 +0530

    format code to improve readability

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -4,29 +4,31 @@ let containerDiv = document.querySelector("#container");//is selected because th
 
 let button = document.querySelector("button");//button is selected for adding eventlistener
 
-button.addEventListener("click", ()=>{
-  let numVariable = parseInt(prompt("What should be the grid size?"))//parseint to convert user input to number
+button.addEventListener("click", ()=>
+  {
+    let numVariable = parseInt(prompt("What should be the grid size?"))//parseint to convert user input to number
+
+    for (let i=0; i<(numVariable*numVariable); i++) 
+    {
+      let createdDiv =document.createElement("div");
+      createdDiv.classList.add("gridDiv");
+
+      //adding length and breadth to createdDiv
+      createdDiv.style.width = (((500/numVariable)/500)*100)+"%";
+      createdDiv.style.paddingBottom = (((500/numVariable)/500*100)+"%");
 
 
-  for (let i=0; i<(numVariable*numVariable); i++) {
-    let createdDiv =document.createElement("div");
-    createdDiv.classList.add("gridDiv");
+      //to change bgcolor of grid divs on mouse hover
+      createdDiv.addEventListener('mouseenter', (e) => 
+        {
+          e.target.style.backgroundColor = generateRandomRgbColor();
+        }
+      );
 
-    //ADDING LENGTH AND BREADTH TO createdDiv
-    createdDiv.style.width = (((500/numVariable)/500)*100)+"%";
-    createdDiv.style.paddingBottom = (((500/numVariable)/500*100)+"%");
-
-
-    //TO CHANGE BGCOLOR OF GRID DIVS ON MOUSE HOVER
-    createdDiv.addEventListener('mouseenter', (e) => {
-    e.target.style.backgroundColor = generateRandomRgbColor();});
-
-    containerDiv.appendChild(createdDiv);
-  }
-
-
+      containerDiv.appendChild(createdDiv);
+    }
   } //arrow function ends here
-) //addeventlistener method ends here
+); //addeventlistener method ends here
 
 //function to generate random colors
 function generateRandomRgbColor() {
