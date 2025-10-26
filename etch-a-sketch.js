6f74299e2953950032fed4c7a9520778f35f8586
Vaibhav Vishwakarma <vaibhav.vishwa1309@gmail.com>
2025-10-26 13:41:35 +0530

    add variable to handle user input and random color function

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,5 +1,27 @@
 let containerDiv = document.querySelector("#container");
 
+
+let numVariable;
+
+let button = document.querySelector("button");
+button.addEventListener("click", ()=>{
+  numVariable = parseInt(prompt("What should be the grid size?"))
+  }
+)
+
+
+function generateRandomRgbColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+
+//TAKING INPUT FROM THE USER TO CREATE GRID DIVS
+
+
+
  
 for (let i=0; i<256; i++) {
     let createdDiv =document.createElement("div");
@@ -8,7 +30,7 @@ for (let i=0; i<256; i++) {
 
 
     createdDiv.addEventListener('mouseenter', (e) => {
-    e.target.style.backgroundColor = 'green';});
+    e.target.style.backgroundColor = generateRandomRgbColor();});
 
     // createdDiv.addEventListener('mouseleave', (e) => {
     // e.target.style.backgroundColor = 'red';});
@@ -17,5 +39,7 @@ for (let i=0; i<256; i++) {
     containerDiv.appendChild(createdDiv);
 
 }
+
+
 
 
