127814484351b8da89a930814c735fa74ce49dc8
Vaibhav Vishwakarma <vaibhav.vishwa1309@gmail.com>
2025-10-26 14:41:04 +0530

    add logic for width padding calculation

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,15 +1,34 @@
-let containerDiv = document.querySelector("#container");
+let containerDiv = document.querySelector("#container");//is selected because the divs would be creted inside this element
 
+// let numVariable;//declared globally to get input for further processing outside the button eventlistener
 
-let numVariable;
+let button = document.querySelector("button");//button is selected for adding eventlistener
 
-let button = document.querySelector("button");
 button.addEventListener("click", ()=>{
-  numVariable = parseInt(prompt("What should be the grid size?"))
+  let numVariable = parseInt(prompt("What should be the grid size?"))//parseint to convert user input to number
+
+
+  for (let i=0; i<(numVariable*numVariable); i++) {
+    let createdDiv =document.createElement("div");
+    createdDiv.classList.add("gridDiv");
+
+    //ADDING LENGTH AND BREADTH TO createdDiv
+    createdDiv.style.width = (((500/numVariable)/500)*100)+"%";
+    createdDiv.style.paddingBottom = (((500/numVariable)/500*100)+"%");
+
+
+    //TO CHANGE BGCOLOR OF GRID DIVS ON MOUSE HOVER
+    createdDiv.addEventListener('mouseenter', (e) => {
+    e.target.style.backgroundColor = generateRandomRgbColor();});
+
+    containerDiv.appendChild(createdDiv);
   }
-)
 
 
+  } //arrow function ends here
+) //addeventlistener method ends here
+
+//function to generate random colors
 function generateRandomRgbColor() {
   const r = Math.floor(Math.random() * 256);
   const g = Math.floor(Math.random() * 256);
@@ -18,27 +37,7 @@ function generateRandomRgbColor() {
 }
 
 
-//TAKING INPUT FROM THE USER TO CREATE GRID DIVS
 
-
-
- 
-for (let i=0; i<256; i++) {
-    let createdDiv =document.createElement("div");
-    createdDiv.classList.add("gridDiv");
-
-
-
-    createdDiv.addEventListener('mouseenter', (e) => {
-    e.target.style.backgroundColor = generateRandomRgbColor();});
-
-    // createdDiv.addEventListener('mouseleave', (e) => {
-    // e.target.style.backgroundColor = 'red';});
-
-
-    containerDiv.appendChild(createdDiv);
-
-}
 
 
 
