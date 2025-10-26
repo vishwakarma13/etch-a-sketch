7fbbbf99352f70dd3ee04dcbf1da262d374b6297
Vaibhav Vishwakarma <vaibhav.vishwa1309@gmail.com>
2025-10-26 20:11:54 +0530

    add validation for input to be between 1 and 100

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -6,27 +6,29 @@ let button = document.querySelector("button");//button is selected for adding ev
 
 button.addEventListener("click", ()=>
   {
-    let numVariable = parseInt(prompt("What should be the grid size?"))//parseint to convert user input to number
+    let numVariable = parseInt(prompt("Enter a number between 1 to 100 :"))//parseint to convert user input to number
 
-    for (let i=0; i<(numVariable*numVariable); i++) 
-    {
-      let createdDiv =document.createElement("div");
-      createdDiv.classList.add("gridDiv");
+    if (numVariable<=100) {
 
-      //adding length and breadth to createdDiv
-      createdDiv.style.width = (((500/numVariable)/500)*100)+"%";
-      createdDiv.style.paddingBottom = (((500/numVariable)/500*100)+"%");
+      for (let i=0; i<(numVariable*numVariable); i++) 
+      {
+        let createdDiv =document.createElement("div");
+        createdDiv.classList.add("gridDiv");
+
+        //adding length and breadth to createdDiv
+        createdDiv.style.width = (((500/numVariable)/500)*100)+"%";
+        createdDiv.style.paddingBottom = (((500/numVariable)/500*100)+"%");
 
 
-      //to change bgcolor of grid divs on mouse hover
-      createdDiv.addEventListener('mouseenter', (e) => 
-        {
-          e.target.style.backgroundColor = generateRandomRgbColor();
-        }
-      );
-
-      containerDiv.appendChild(createdDiv);
-    }
+        //to change bgcolor of grid divs on mouse hover
+        createdDiv.addEventListener('mouseenter', (e) => 
+          {
+            e.target.style.backgroundColor = generateRandomRgbColor();
+          }
+        );
+        containerDiv.appendChild(createdDiv);
+      }
+    }  
   } //arrow function ends here
 ); //addeventlistener method ends here
 
